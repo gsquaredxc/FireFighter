8940464c95fca0eb8c9eff73ef3718ad3de00373
gsquaredxc <ggarrison5@gmail.com>
2020-07-18 12:21:13 -0500

    I did the big dumb

--- configUtils.py
+++ configUtils.py
@@ -3,11 +3,11 @@ import config
 
 def get_value(guildID, key):
     try:
-        return config.Config(str(guildID))[key]
+        return config.Config[str(guildID)][key]
     except:
-        config.Config(str(guildID))[key] = None
+        config.Config[str(guildID)][key] = None
         return None
 
 
 def set_value(guildID, key, value):
-    config.Config(str(guildID))[key] = value
+    config.Config[str(guildID)][key] = value

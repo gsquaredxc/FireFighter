66a7cba658f5b7054495c01bac1a5f50434381ef
gsquaredxc <ggarrison5@gmail.com>
2020-07-20 01:58:10 -0500

    fix bug

--- userPerms.py
+++ userPerms.py
@@ -14,8 +14,8 @@ async def has_admin_perms_server(member, guild):
 
 
 async def check_if_can_delete(member, guild):
-    return globalVars.gConfig.get_value(guild,"allow_delete_all") or await has_admin_perms_server(member, guild)
+    return globalVars.gConfig.get_value(guild.id,"allow_delete_all") or await has_admin_perms_server(member, guild)
 
 
 async def check_if_can_ban(member, guild):
-    return globalVars.gConfig.get_value(guild,"allow_ban_all") or await has_admin_perms_server(member, guild)
+    return globalVars.gConfig.get_value(guild.id,"allow_ban_all") or await has_admin_perms_server(member, guild)
